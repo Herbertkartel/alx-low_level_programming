bf2c0160470e2308ad082193481924b23f066074
Herbertkartel <herbzkartel@gmail.com>
2022-09-09 04:09:09 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
-
-/**
- * main - Prints exactly "and that peice of art is useful" - Dora Korpar, 2015-10-19",
- * Followed by a new line, to the standard error.
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-	return (1);
-}
+Unix
