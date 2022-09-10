275b4c121ac7eac8621a0b1f84c670a6bd2668f7
Herbertkartel <herbzkartel@gmail.com>
2022-09-10 04:13:19 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,11 +1,8 @@
-/*
- * File: 10-print_combo2.c
- * Auth: Herbert
- */
-
 #include <stdio.h>
+
 /**
- * main -  Prints the numbers from 00 to 99, numbers separated 
+ *  * main - Prints the numbers from 00 to 99, numbers separated by
+ *           a comma followed by a space, in ascending order.
  *
  * Return: Always 0.
  */
@@ -18,7 +15,7 @@ for (digit2 = 0; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
-if (gigit1 == 9 && digit2 == 9)
+if (digit1 == 9 && digit2 == 9)		
 continue;
 putchar(',');
 putchar(' ');
