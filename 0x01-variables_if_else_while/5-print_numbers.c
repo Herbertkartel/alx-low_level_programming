aa8ceaac03135068868d0f90c43b00316afde01b
Herbertkartel <herbzkartel@gmail.com>
2022-09-10 00:58:27 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,10 +7,11 @@
  *Return: Always 0 (Success)
  */
 int main(void)
-{	
-int i;
-for (i = 0; i < 10; i++)
-printf("%d", i);
+{
+int num;
+
+for (num = 0; num < 10; num++)
+printf("%d", num);
 printf("\n");
 return (0);
 }
