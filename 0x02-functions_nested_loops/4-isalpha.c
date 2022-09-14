6dc9453c07d9261fed0116eb58e04ddf49a12ece
Herbertkartel <herbzkartel@gmail.com>
2022-09-14 02:58:05 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * print _isalpha - Checks for alphabetic character
  * @c: The character to be checked
  *
- * Return: Always 0
+ * Return: 1 for alphabetic character or 0 for anything else
  */
 int _isalpha(int c)
 {
