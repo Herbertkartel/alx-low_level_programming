fd8acfb72af49a596b2748d28e0bec02c6ff3b4e
Herbertkartel <herbzkartel@gmail.com>
2022-09-14 02:09:38 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,15 @@
  */
 void print_alphabet_x10(void)
 {
-char c, i;
-for (i = 0; i <= 9; i++)
+char c;
+int i;
+while (i <= 9)
 {
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
 _putchar('\n');
+i++;
 }
 }
