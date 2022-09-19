3162bbdd558e2c035a54cdf8a38f3c977a7820aa
Herbertkartel <herbzkartel@gmail.com>
2022-09-19 04:31:55 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,12 @@
 /**
 * _isupper - checks if parameter is uppercase character
 * @c: character to be tested
-*
 * Return: 1 if c is uppercase.
 */
 
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
+if ((c >= 'A') && c <= 'Z'))
 {
 return (1);
 }
