37cdfdd0ce84b8ce493fd1392195886bcd9ed198
Herbertkartel <herbzkartel@gmail.com>
2022-09-28 12:07:20 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,11 +8,11 @@
  * Return: -1 or sqrt of c
  */
 
-int sqrt_check(int g, int c)
+int _sqrt_recursion(int n);
 {
-if (g * g == c)
-return (g);
-if (g * g > c)
+if (n * n == 0)
+return (n);
+
 return (-1);
-return (sqrt_check(g + 1, c));
+return (sqrt_recursion(n + 1, n));
 }
