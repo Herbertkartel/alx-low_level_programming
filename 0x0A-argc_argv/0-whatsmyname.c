9a6bf0cacfbe9768f3dee11c3c5448d0987a7c0e
Herbertkartel <herbzkartel@gmail.com>
2022-10-01 13:02:55 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,13 @@
 /**
  * main - Prints the name of the program
  * @argc: Number of command line arguments
- * @argv: Array name
+ * @argv: argument vector of pointers to strings
  *
+ * Return: always 0
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-printf("%s\n", argv[argc * 0]);
+printf("%s\n", argv[0]);
 return (0);
 }
