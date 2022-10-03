2fd89f1086a68635fb266b755e6c9a538ea362f3
Herbertkartel <herbzkartel@gmail.com>
2022-10-03 02:42:01 -0700

    Task 0

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,12 +17,12 @@ int a, b;
 if (str == 0)
 return (NULL);
 for (a = 0; str[a] != '\0'; a++)
-;
+	;
 a++;
 s = malloc(a * sizeof(*s));
 if (s == 0)
 return (NULL);
-for (b = 0; b < a; b++);
+for (b = 0; b < a; b++)
 s[b] = str[b];
 return (s);
 }
