40f9d8fcd7be98b060b235b8fdfaf561195e9c70
Herbertkartel <herbzkartel@gmail.com>
2022-10-13 01:10:44 -0700

    Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,5 +1,5 @@
+#include <stdlib.h> 
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - print opcodes of this function
@@ -10,26 +10,40 @@
  * Return: Always 0
  */
 
-int main(int argc, char *argv[])
+void print_opcodes(char *a, int n)
 {
-int b;
-char *mem = (char *) main;
+int i;
+
+for (i = 0; i < n; i++)
+{
+printf("%.2hhx", a[i]);
+if (i < n - 1)
+printf(" ");
+}
+printf("\n");
+
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ */
+int main(int argc, char **argv)
+{
+int n;
 
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
-
-b = atoi(argv[1]);
-
-if (b < 0)
+n = atoi(argv[1]);
+if (n < 0)
 {
 printf("Error\n");
 exit(2);
 }
-while (b--);
-printf("%02x%c", *mem++ & 0xff, b ? ' ' : '\n');
-
+print_opcodes((char *)&main, n);
 return (0);
 }
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,3 +1,1 @@
-[master 7b23cb5] print opcodes of this function
- 2 files changed, 36 insertions(+), 1 deletion(-)
- create mode 100644 0x0F-function_pointers/100-main_opcodes.c
+Print the opcodes of this program
