1befd2a3eed2c72267fafad863af34e55a70c812
Herbertkartel <herbzkartel@gmail.com>
2022-10-24 23:45:26 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h);
 {
-size_t i = 0;
+size_t nnodes = 0;
 
-while (h)
+while (h != NULL)
 {
 printf("%d\n", h->n);
 h = h->next;
-++i;
+nnodes++;
 }
-return (i);
+return (nnodes);
 }
 
